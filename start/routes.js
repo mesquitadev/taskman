9a73e9967d06827395e764372858dd829b3fb7c1
Victor Mesquita <mesquitadev@gmail.com>
2020-12-10 10:56:57 -0300

    M - Repair Route

--- start/routes.js
+++ start/routes.js
@@ -4,25 +4,26 @@ const Route = use('Route')
 const Env = use('Env')
 
 Route.get('/', ({ response }) => {
-  return response.redirect('/api')
+  return response.redirect('/api/v1')
 })
 
-Route.post('users', 'UserController.store').validator('User')
-
-Route.post('sessions', 'SessionController.store').validator(['Session'])
-
-Route.post('forgot', 'ForgotPasswordController.store').validator(['ForgotPassword'])
-
-Route.put('reset', 'ForgotPasswordController.update').validator(['ResetPassword'])
 Route.group(() => {
   Route.get('/', () => {
     return {
       success: `Server running on ${Env.get('HOST')} in port: ${Env.get('PORT')}`
     }
   })
+
+  Route.post('users', 'UserController.store').validator('User')
+
+  Route.post('sessions', 'SessionController.store').validator(['Session'])
+
+  Route.post('forgot', 'ForgotPasswordController.store').validator(['ForgotPassword'])
+
+  Route.put('reset', 'ForgotPasswordController.update').validator(['ResetPassword'])
   Route.post('/files', 'FileController.store')
 
-  Route.get('/files/:id', 'FileController.show')
+  Route.get('/files/:id', 'FileController.show').middleware(['auth'])
 
   Route.resource('projects', 'ProjectController')
     .apiOnly()
@@ -33,7 +34,7 @@ Route.group(() => {
           ['Project']
         ]
       ]
-    ))
+    )).middleware(['auth']).apiOnly()
   Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map(
     [
       [
@@ -41,5 +42,5 @@ Route.group(() => {
         ['Task']
       ]
     ]
-  ))
-}).prefix('api').middleware(['auth'])
+  )).middleware(['auth'])
+}).prefix('api/v1')
