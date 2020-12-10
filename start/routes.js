2fe6e293cdccd7e210c718fa682f613cb6d65c82
Victor Mesquita <mesquitadev@gmail.com>
2020-12-10 10:30:06 -0300

    M - Routes - Adding Namespace Routes

--- start/routes.js
+++ start/routes.js
@@ -1,36 +1,49 @@
 'use strict'
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const Env = use('Env')
 
-Route.post('users', 'UserController.store').validator('User')
-
-Route.post('sessions', 'SessionController.store').validator(['Session'])
-
-Route.post('forgot', 'ForgotPasswordController.store').validator(['ForgotPassword'])
-
-Route.put('reset', 'ForgotPasswordController.update').validator(['ResetPassword'])
+Route.get('/', ({ response }) => {
+  return response.redirect('/api')
+})
 
 Route.group(() => {
-  Route.post('/files', 'FileController.store')
+  Route.get('/', () => {
+    return {
+      success: `Server running on ${Env.get('HOST')} in port: ${Env.get('PORT')}`
+    }
+  })
 
-  Route.get('/files/:id', 'FileController.show')
+  Route.post('users', 'UserController.store').validator('User')
 
-  Route.resource('projects', 'ProjectController')
-    .apiOnly()
-    .validator(new Map(
+  Route.post('sessions', 'SessionController.store').validator(['Session'])
+
+  Route.post('forgot', 'ForgotPasswordController.store').validator(['ForgotPassword'])
+
+  Route.put('reset', 'ForgotPasswordController.update').validator(['ResetPassword'])
+
+  Route.group(() => {
+    Route.post('/files', 'FileController.store')
+
+    Route.get('/files/:id', 'FileController.show')
+
+    Route.resource('projects', 'ProjectController')
+      .apiOnly()
+      .validator(new Map(
+        [
+          [
+            ['projects.store'],
+            ['Project']
+          ]
+        ]
+      ))
+    Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map(
       [
         [
-          ['projects.store'],
-          ['Project']
+          ['projects.tasks.store'],
+          ['Task']
         ]
       ]
     ))
-  Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map(
-    [
-      [
-        ['projects.tasks.store'],
-        ['Task']
-      ]
-    ]
-  ))
-}).middleware(['auth'])
+  }).middleware(['auth'])
+}).prefix('api')
